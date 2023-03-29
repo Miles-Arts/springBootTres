6a477e6ebe453cef6779119b9a7eee3f6fe43b29
Milton Figueredo Miles <gratno@gmail.com>
2023-03-28 22:10:03 -0500

    Hello Controller

--- src/main/java/med/voll/api/HelloController.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api;
-
-public class HelloController {
-
-
-
-}
---
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -0,0 +1,11 @@
+package med.voll.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  //Anotación @RestController
+public class HelloController {
+
+
+
+}
