0ae776df4d37276660b487c7f149ee469ba6b6ec
Milton Figueredo Miles <gratno@gmail.com>
2023-03-29 19:23:48 -0500

    Creando API Rest

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,22 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+
+public class MedicoController {
+
+    @PostMapping
+    public void resgitrarMedico(@RequestBody String parametro) {
+
+        System.out.println("El request llega correctamente");
+
+        System.out.println(parametro);
+
+    }
+
+}
