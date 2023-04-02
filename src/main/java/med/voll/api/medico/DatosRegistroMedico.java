1b182f517efdb8144ce8a11e6dad42e8b64fdbb3
Milton Figueredo Miles <gratno@gmail.com>
2023-04-01 19:10:06 -0500

    Error

--- src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -2,8 +2,12 @@ package med.voll.api.medico;
 
 import med.voll.api.direccion.DatosDireccion;
 
-public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad, DatosDireccion datosdireccion) {
-
-
+public record DatosRegistroMedico(
+        String nombre,
+        String email,
+        String documento,
+        Especialidad especialidad,
+        DatosDireccion datosdireccion
+) {
 
 }
