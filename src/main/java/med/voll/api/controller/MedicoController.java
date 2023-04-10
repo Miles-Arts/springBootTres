4cb03e73551e97889bdfaede8852268dc1c215b8
Milton Figueredo Miles <gratno@gmail.com>
2023-04-10 16:00:13 -0500

    Paginación #2

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,13 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.medico.DatosListadoMedico;
 import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,14 +21,15 @@ public class MedicoController {
 
 
     @PostMapping
-    public void registrarMedico(@RequestBody @Valid  DatosRegistroMedico datosRegistroMedico) {
+    public void registrarMedico(@Valid  @RequestBody  DatosRegistroMedico datosRegistroMedico) {
 
         medicoRepository.save(new Medico(datosRegistroMedico));
     }
 
     @GetMapping
-    public List<Medico> listadoMedicos() {
-        return medicoRepository.findAll();
+    //public List<DatosListadoMedico> listadoMedicos()
+    public Page<DatosListadoMedico> listadoMedicos(Pageable paginacion) {
+        return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
     }
 
 }
