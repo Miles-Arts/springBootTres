835e649d1548d6fbc237a6a5178381743d0ab889
Milton Figueredo Miles <gratno@gmail.com>
2023-04-10 17:39:25 -0500

    Actualizando datos #1

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DatosListadoMedico;
-import med.voll.api.medico.DatosRegistroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -33,4 +30,9 @@ public class MedicoController {
         return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
     }
 
+    @PutMapping
+    public void actualizarMedico(DatosActualizarMedico datosActualizarMedico) {
+       Medico medico = medicoRepository.getReferenceById(datosActualizarMedico.id());
+
+    }
 }
